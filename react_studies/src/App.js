aaa330bc416f4bbdec1542e45af3f55caa70f38c
LuizHenriqueGomesRibeiro <louisnuuk@hotmail.com>
2024-01-15 21:25:29 -0300

    feat: program created

---
+++ react_studies/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Styled_component from './components/styled/Styled_component';
+
+// eslint-disable-next-line react/jsx-pascal-case
+function App() {
+  return (
+    <div className="App">
+      <Styled_component/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react_studies/src/components/styled/Styled_component.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './styled.module.css'
+
+export default function Styled_component() {
+  return (
+    <div>
+        <p>Olá react</p>
+    </div>
+  )
+}

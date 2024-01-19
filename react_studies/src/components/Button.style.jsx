d6f18892c3bac3c798669d0786aa9461e36f1274
LuizHenriqueGomesRibeiro <louisnuuk@hotmail.com>
2024-01-18 21:37:31 -0300

    feat: finish studies today

--- react_studies/src/components/Button.style.jsx
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components'
-
-export const StyledButton = styled.button`
-    background-color: red;
-    width: 100px;
-    height: 100px;
-`
---
+++ studies/src/App.js
@@ -0,0 +1,16 @@
+import { Button, Div } from './components/Button'
+
+function App(){
+  return <Div>
+          <Button>
+            <p>
+              React deu um olá!
+            </p>
+            <div>
+              
+            </div>
+          </Button>
+        </Div>
+}
+
+export default App
